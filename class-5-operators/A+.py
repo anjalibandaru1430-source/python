d63afff4d8caecc58822777332c83c03653aa7ca
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-03-24 13:06:35 +0530

    py

---
+++ class-5-operators/A+.py
@@ -0,0 +1,18 @@
+#  + - ADDITION  AND CONCATINATION
+
+a = 10
+b = 20.5
+c = "Rahul"
+d = "30"
+e = True
+f = False
+
+print(a+b) #30.5
+print(a+e) #10+1 = 11
+print(a+f) #10+0 = 10
+print(d+c) #"30Rahul"
+print(a+c) #typeError
+print(a+d) #typeError
+
+
+
---
+++ class-5-operators/A-mul.py
@@ -0,0 +1,8 @@
+#  * - MULTIPLY AND REPEATING
+
+price = 30
+qty = 2
+p_name = "Pen"
+
+print(price*qty)
+print(p_name*qty)
