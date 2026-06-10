8562127e1737553bffc52b55234d6b5fe6d95066
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-06-10 12:08:47 +0530

    python

--- class-4-/set.py
+++ class-4-/set.py
@@ -10,3 +10,11 @@ s1=set()
 s2={101,102,102,103}
 print(s1)
 print(s2)
+print(type(s1))
+print(type(s2))
+
+
+
+#if we want maintain duplicates "if order matters" we can use list
+#if we want to maitain unique values and order does not matter we can use set
+#
