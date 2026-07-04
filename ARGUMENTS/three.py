7f93adaae81f90e8b1d2a2ca43461a523208204e
anjalibandaru1430-source <anjalibandaru1430@gmail.com>
2026-07-04 22:26:58 +0530

    py

--- ARGUMENTS/three.py
+++ ARGUMENTS/three.py
@@ -6,3 +6,20 @@ substraction(a=100,b=200)
 
 
 
+
+def substraction(a,b):
+    print("sub",a-b)
+
+substraction(a=200,b=190)
+substraction(a=100,b=200)
+
+
+
+def substraction(a,b):
+    print("sub",a-b)
+
+substraction(a=200,b=1060)
+substraction(a=100,b=200)
+
+
+
